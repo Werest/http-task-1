3c568591daad24f05520d9b67613befa102a8cca
Konstantin Chigridov <werest54@rambler.ru>
2023-03-30 08:40:21 +0300

    Initial commit

---
+++ src/main/java/ru/werest/http/CatFacts.java
@@ -0,0 +1,45 @@
+package ru.werest.http;
+
+
+//id - уникальный идентификатор записи
+//text - сообщение
+//type - тип животного
+//user - имя пользователя
+//upvotes - голоса
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CatFacts {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final Integer upvotes;
+
+    public CatFacts(@JsonProperty("id") String id,
+                    @JsonProperty("text") String text,
+                    @JsonProperty("type") String type,
+                    @JsonProperty("user") String user,
+                    @JsonProperty("upvotes") int upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "CatFacts{" +
+                "id='" + id + '\'' +
+                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
+                ", user='" + user + '\'' +
+                ", upvotes=" + upvotes +
+                '}';
+    }
+}
